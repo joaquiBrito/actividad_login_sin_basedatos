eca9968f08fc45944a4c431cdd8e16fbfd19df7b
joaquin brito <sergiojoaquinbritohotmail.com>
2022-11-10 20:29:23 -0300

    [LOGIN]

---
+++ Login/Login/frmLogin.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Login
+{
+    public partial class frmLogin : Form
+    {
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnIngresar_Click(object sender, EventArgs e)
+        {
+            if(txtUsuario.Text == "")
+            {
+                MessageBox.Show("ingresar Usuario");
+                return;
+                txtUsuario.Focus();
+            }
+            if (txtClave.Text == "")
+            {
+                MessageBox.Show("ingresar Clave");
+                return;
+                txtClave.Focus();
+            }
+            MessageBox.Show("ingreso al sistema ");
+            this.Close();   
+        }
+    }
+}
